49d47c8fffc77fbc051b4fd2f530bcb3da4d1e9b
Lawi-Mwaura <lawimwaura@gmail.com>
2023-09-23 14:46:53 +0300

    Updated code

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -4,10 +4,23 @@ import json
 class GetRequester:
 
     def __init__(self, url):
+        url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(url="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+        return response.content
+        
 
     def load_json(self):
-        pass
+        json_list = []
+        json_list = json.loads(self.get_response_body)
+        for json_list in json_list:
+            json_list.append()
+        return json_list
+    
+
+    load_json = json_list.loadjson()
+    for list in set (load_json):
+        print (json_list)
+    
